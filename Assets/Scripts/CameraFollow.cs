619531c9c102d603f24078bfa5ac052c7157e2b1
ertuakkaya <ertuakkaya@gmail.com>
2024-02-13 16:10:11 +0300

    player movement try
    player can move
    camera is following player
    player movement should fixed

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform playerTransform;
+    private Vector3 offset;
+    public float smoothSpeed = 0.125f; // Add this line
+
+    void Start()
+    {
+        // Calculate the initial offset.
+        offset = transform.position - playerTransform.position;
+    }
+
+    void LateUpdate()
+    {
+        // Update the camera's position to follow the player, without changing rotation.
+        Vector3 desiredPosition = playerTransform.position + offset;
+        Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
+        transform.position = smoothedPosition;
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed;
+    [SerializeField] private float turnSpeed;
+    [SerializeField] private float jumpForce;
+
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        Vector3 moveDirection = (Camera.main.transform.forward * verticalInput) + (Camera.main.transform.right * horizontalInput);
+        moveDirection.y = 0;
+
+        rb.velocity = moveDirection * moveSpeed;
+
+        Quaternion targetRotation = Quaternion.LookRotation(moveDirection);
+
+        rb.MoveRotation(Quaternion.Slerp(rb.rotation, targetRotation, turnSpeed * Time.deltaTime));
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        }
+    }
+}
