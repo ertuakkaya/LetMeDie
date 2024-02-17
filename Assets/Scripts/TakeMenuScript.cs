e9bb7df682716f1e370fbe072e418a3ef4622f1a
Dilara <dilaraozturk5881@gmail.com>
2024-02-17 22:35:03 +0300

    bir şeyler denedim

---
+++ Assets/Scripts/TakeMenuScript.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class TakeMenuScript : MonoBehaviour
+{
+    public void MenuButton()
+    {
+        SceneManager.LoadScene(1);
+    }
+    
+}
