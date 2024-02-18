f194b250f8b18ad51641c24b2d089a27df37cba7
Dilara <dilaraozturk5881@gmail.com>
2024-02-18 15:17:43 +0300

    Main menu fixed

--- Assets/Scripts/GameOver.cs
+++ Assets/Scripts/GameOver.cs
@@ -12,7 +12,7 @@ public class GameOver : MonoBehaviour
 
    public void MainMenuButton()
     {
-        SceneManager.LoadScene("Dilara");
+        SceneManager.LoadScene("DilaraCopy");
 
 
     } 
